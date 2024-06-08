cca15721d47bf3fa41d31b1ebef41c790aa889ad
thomas200593 <thomas200593@yahoo.com>
2024-06-09 01:59:50 +0700

    Remove Window Size Class Merge App bar to corresponded Screen

--- app/src/main/java/com/thomas200593/mini_retail_app/features/dashboard/navigation/DashboardNavGraph.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/dashboard/navigation/DashboardNavGraph.kt
@@ -1,26 +1,12 @@
 package com.thomas200593.mini_retail_app.features.dashboard.navigation
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.thomas200593.mini_retail_app.R
 import com.thomas200593.mini_retail_app.app.navigation.NavigationGraphs.G_DASHBOARD
 import com.thomas200593.mini_retail_app.app.navigation.ScreenGraphs
-import com.thomas200593.mini_retail_app.core.ui.common.Icons
-import com.thomas200593.mini_retail_app.core.ui.component.AppBar
 import com.thomas200593.mini_retail_app.features.business.navigation.businessNavGraph
 import com.thomas200593.mini_retail_app.features.dashboard.ui.DashboardScreen
 import com.thomas200593.mini_retail_app.features.report_analysis.navigation.reportAnalysisNavGraph
--- app/src/main/java/com/thomas200593/mini_retail_app/features/report_analysis/navigation/ReportAnalyisisNavGraph.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/report_analysis/navigation/ReportAnalyisisNavGraph.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.thomas200593.mini_retail_app.app.navigation.NavigationGraphs
 import com.thomas200593.mini_retail_app.app.navigation.NavigationGraphs.G_REPORT_ANALYSIS
 import com.thomas200593.mini_retail_app.app.navigation.ScreenGraphs
 import com.thomas200593.mini_retail_app.features.report_analysis.ui.ReportAnalysisScreen
