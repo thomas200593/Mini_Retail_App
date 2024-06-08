a4df57345d2ba9e82573a8da0f7a229861151934
thomas200593 <thomas200593@yahoo.com>
2024-06-09 01:33:38 +0700

    Package Refactoring

--- app/src/main/java/com/thomas200593/mini_retail_app/features/dashboard/ui/DashboardScreen.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/dashboard/ui/DashboardScreen.kt
@@ -1,13 +1,7 @@
 package com.thomas200593.mini_retail_app.features.dashboard.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
 
 @Composable
 fun DashboardScreen(
