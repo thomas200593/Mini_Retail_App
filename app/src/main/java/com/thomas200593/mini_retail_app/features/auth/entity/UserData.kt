b9ae6c4492892919e8f685596d98f782d4c3f392
thomas200593 <thomas200593@yahoo.com>
2024-07-03 03:23:12 +0700

    Zipping & tidying up

--- app/src/main/java/com/thomas200593/mini_retail_app/features/auth/entity/UserData.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/auth/entity/UserData.kt
@@ -1,10 +1,5 @@
 package com.thomas200593.mini_retail_app.features.auth.entity
 
-data class UserData(
-    val authSessionToken: AuthSessionToken?,
-    val oAuth2UserMetadata: OAuth2UserMetadata?,
-)
-
 sealed class OAuth2UserMetadata{
     data class Google(
         val email: String,
@@ -14,3 +9,8 @@ sealed class OAuth2UserMetadata{
         val expiredAt: String
     ) : OAuth2UserMetadata()
 }
+
+data class UserData(
+    val authSessionToken: AuthSessionToken?,
+    val oAuth2UserMetadata: OAuth2UserMetadata?,
+)
