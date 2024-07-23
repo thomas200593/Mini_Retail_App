ff047bd9c5a94a6da9e3b494ccd7c6f5d5dbe754
thomas200593 <thomas200593@yahoo.com>
2024-07-23 16:08:21 +0700

    Breakdown App Config to Modular & Simplifying Variables

--- app/src/main/java/com/thomas200593/mini_retail_app/app/navigation/Routes.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/app/navigation/Routes.kt
@@ -5,56 +5,56 @@ object Routes {
     /**
      * Initial
      */
-    const val R_INITIAL: String = "r_initial"
-    const val R_INITIALIZATION: String = "r_initialization"
+    const val R_INITIAL = "r_initial"
+    const val R_INITIALIZATION = "r_initialization"
 
     /**
      * Onboarding
      */
-    const val R_ONBOARDING: String = "r_onboarding"
+    const val R_ONBOARDING = "r_onboarding"
 
     /**
      * Auth
      */
-    const val R_AUTH: String = "r_auth"
+    const val R_AUTH = "r_auth"
 
     /**
      * App Config
      */
-    const val R_APP_CONFIG: String = "r_app_config"
+    const val R_APP_CONFIG = "r_app_config"
     //App Config General
-    const val R_CONFIG_GENERAL: String = "r_config_general"
-    const val R_COUNTRY: String = "r_country"
-    const val R_LANGUAGE: String = "r_language"
-    const val R_TIMEZONE: String = "r_timezone"
-    const val R_THEME: String = "r_theme"
-    const val R_DYNAMIC_COLOR: String = "r_dynamic_color"
-    const val R_FONT_SIZE: String = "r_font_size"
-    const val R_CURRENCY: String = "r_currency"
+    const val R_CONFIG_GENERAL = "r_config_general"
+    const val R_COUNTRY = "r_country"
+    const val R_LANGUAGE = "r_language"
+    const val R_TIMEZONE = "r_timezone"
+    const val R_THEME = "r_theme"
+    const val R_DYNAMIC_COLOR = "r_dynamic_color"
+    const val R_FONT_SIZE = "r_font_size"
+    const val R_CURRENCY = "r_currency"
     //App Config Data
-    const val R_CONFIG_DATA: String = "r_config_data"
-    const val R_DATA_BACKUP: String = "r_data_backup"
+    const val R_CONFIG_DATA = "r_config_data"
+    const val R_DATA_BACKUP = "r_data_backup"
 
     /**
      * Dashboard
      */
-    const val R_DASHBOARD: String = "r_dashboard"
+    const val R_DASHBOARD = "r_dashboard"
 
     /**
      * Business
      */
-    const val R_BUSINESS: String = "r_business"
-    const val R_MASTER_DATA: String = "r_master_data"
-    const val R_SUPPLIER: String = "r_supplier"
-    const val R_CUSTOMER: String = "r_customer"
+    const val R_BUSINESS = "r_business"
+    const val R_MASTER_DATA = "r_master_data"
+    const val R_SUPPLIER = "r_supplier"
+    const val R_CUSTOMER = "r_customer"
 
     /**
      * Reporting
      */
-    const val R_REPORTING: String = "r_reporting"
+    const val R_REPORTING = "r_reporting"
 
     /**
      * User Profile
      */
-    const val R_USER_PROFILE: String = "r_user_profile"
+    const val R_USER_PROFILE = "r_user_profile"
 }
