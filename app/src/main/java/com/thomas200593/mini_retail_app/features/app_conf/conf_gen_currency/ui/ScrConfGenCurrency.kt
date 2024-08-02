d0d3d3793938f36f9bf1fdc920b96822025ea509
thomas200593 <thomas200593@yahoo.com>
2024-08-02 18:32:34 +0700

    Breakdown App Config to Modular & Simplifying Variables

--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_currency/ui/ScrConfGenCurrency.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_currency/ui/ScrConfGenCurrency.kt
@@ -9,59 +9,120 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.AutoMirrored.Outlined
+import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
+import androidx.compose.ui.text.style.TextAlign.Companion.Start
+import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.thomas200593.mini_retail_app.R
-import com.thomas200593.mini_retail_app.app.ui.StateApp
+import com.thomas200593.mini_retail_app.R.string.str_country
+import com.thomas200593.mini_retail_app.R.string.str_empty_message
+import com.thomas200593.mini_retail_app.R.string.str_empty_message_title
+import com.thomas200593.mini_retail_app.R.string.str_error
+import com.thomas200593.mini_retail_app.R.string.str_error_fetching_preferences
+import com.thomas200593.mini_retail_app.R.string.str_ok
 import com.thomas200593.mini_retail_app.app.ui.LocalStateApp
-import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState
+import com.thomas200593.mini_retail_app.app.ui.StateApp
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Empty
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Error
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Idle
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Loading
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Success
 import com.thomas200593.mini_retail_app.core.ui.common.CustomIcons.Currency.currency
-import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar
+import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar.ProvideTopAppBarAction
+import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar.ProvideTopAppBarNavigationIcon
+import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar.ProvideTopAppBarTitle
+import com.thomas200593.mini_retail_app.core.ui.component.CustomDialog.AlertDialogContext.ERROR
+import com.thomas200593.mini_retail_app.core.ui.component.CustomDialog.AlertDialogContext.INFORMATION
+import com.thomas200593.mini_retail_app.core.ui.component.CustomDialog.AppAlertDialog
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.EmptyScreen
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.ErrorScreen
-import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.LoadingScreen
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.ThreeRowCardItem
-import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.entity.Currency
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.entity.ConfigCurrency
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.entity.Currency
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui.VMConfGenCurrency.UiEvents.BtnSelectCurrencyEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui.VMConfGenCurrency.UiEvents.ButtonEvents.BtnNavBackEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui.VMConfGenCurrency.UiEvents.OnOpenEvents
 
 @Composable
 fun ScrConfGenCurrency(
     vm: VMConfGenCurrency = hiltViewModel(),
     stateApp: StateApp = LocalStateApp.current
 ) {
-    val confData by vm.confData.collectAsStateWithLifecycle()
-
-    TopAppBar(onNavigateBack = stateApp::onNavUp)
-    ScreenContent(
-        configData = confData,
-        onSaveSelectedCurrency = vm::setCurrency
+    val sessionState by stateApp.isSessionValid.collectAsStateWithLifecycle()
+    val uiState by vm.uiState.collectAsStateWithLifecycle()
+    LaunchedEffect(sessionState) { vm.onEvent(OnOpenEvents(sessionState)) }
+    TopAppBar(onNavigateBack = { vm.onEvent(BtnNavBackEvents.OnClick) })
+    when(uiState.configCurrency){
+        Idle, Loading -> Unit
+        Empty -> EmptyScreen(
+            title = stringResource(id = str_empty_message_title),
+            emptyMessage = stringResource(id = str_empty_message),
+            showIcon = true
+        )
+        is Error -> ErrorScreen(
+            title = stringResource(id = str_error),
+            errorMessage = stringResource(id = str_error_fetching_preferences),
+            showIcon = true
+        )
+        is Success -> ScreenContent(
+            configCurrency = (uiState.configCurrency as Success).data,
+            onSaveSelectedCurrency = { vm.onEvent(BtnSelectCurrencyEvents.OnClick(sessionState, it)) }
+        )
+    }
+    AppAlertDialog(
+        showDialog = uiState.dialogState.dlgLoadDataEnabled,
+        dialogContext = INFORMATION,
+        showIcon = true,
+        showTitle = true,
+        title = { Text(text = stringResource(id = R.string.str_loading))},
+        showBody = true,
+        body = { Text(text = stringResource(id = R.string.str_loading))},
+    )
+    AppAlertDialog(
+        showDialog = uiState.dialogState.dlgLoadDataErrorEnabled,
+        dialogContext = ERROR,
+        showIcon = true,
+        showTitle = true,
+        title = { Text(text = stringResource(id = str_error))},
+        showBody = true,
+        body = { Text("Load Data Error") },
+        useConfirmButton = true,
+        confirmButton = {
+            TextButton(onClick = { vm.onEvent(OnOpenEvents(sessionState)) })
+            { Text(stringResource(id = str_ok)) }
+        }
     )
 }
 
 @Composable
 private fun TopAppBar(onNavigateBack: () -> Unit) {
-    CustomAppBar.ProvideTopAppBarNavigationIcon {
+    ProvideTopAppBarNavigationIcon {
         Surface(
             onClick =  onNavigateBack,
             modifier = Modifier
@@ -73,7 +134,7 @@ private fun TopAppBar(onNavigateBack: () -> Unit) {
             )
         }
     }
-    CustomAppBar.ProvideTopAppBarTitle {
+    ProvideTopAppBarTitle {
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
@@ -87,11 +148,11 @@ private fun TopAppBar(onNavigateBack: () -> Unit) {
             Text(
                 text = stringResource(id = R.string.str_currency),
                 maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+                overflow = Ellipsis
             )
         }
     }
-    CustomAppBar.ProvideTopAppBarAction {
+    ProvideTopAppBarAction {
         Row(
             modifier = Modifier.padding(end = 20.dp),
             verticalAlignment = Alignment.CenterVertically,
@@ -99,7 +160,7 @@ private fun TopAppBar(onNavigateBack: () -> Unit) {
         ){
             Icon(
                 modifier = Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize),
-                imageVector = Icons.Default.Info,
+                imageVector = Default.Info,
                 contentDescription = null
             )
         }
@@ -108,97 +169,76 @@ private fun TopAppBar(onNavigateBack: () -> Unit) {
 
 @Composable
 private fun ScreenContent(
-    configData: ResourceState<ConfigCurrency>,
+    configCurrency: ConfigCurrency,
     onSaveSelectedCurrency: (Currency) -> Unit
 ) {
-    when(configData){
-        ResourceState.Idle, ResourceState.Loading -> { LoadingScreen() }
-        ResourceState.Empty -> {
-            EmptyScreen(
-                title = stringResource(id = R.string.str_empty_message_title),
-                emptyMessage = stringResource(id = R.string.str_empty_message),
-                showIcon = true
-            )
-        }
-        is ResourceState.Error -> {
-            ErrorScreen(
-                title = stringResource(id = R.string.str_error),
-                errorMessage = stringResource(id = R.string.str_error_fetching_preferences),
-                showIcon = true
-            )
-        }
-        is ResourceState.Success -> {
-            val currentData = configData.data.configCurrent.currency
-            val preferencesList = configData.data.currencies
-
-            Column(
-                modifier = Modifier.fillMaxSize().padding(4.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "${stringResource(id = R.string.str_currency)} : ${currentData.displayName}",
-                    modifier = Modifier.fillMaxWidth().padding(4.dp),
-                    fontWeight = FontWeight.Bold,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    textAlign = TextAlign.Center,
-                )
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize().padding(4.dp),
-                    verticalArrangement = Arrangement.spacedBy(10.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    items(count = preferencesList.count()){ index ->
-                        val data = preferencesList[index]
-                        ThreeRowCardItem(
-                            firstRowContent = {
-                                Text(
-                                    text = data.code,
-                                    modifier = Modifier.fillMaxWidth(),
-                                    textAlign = TextAlign.Center,
-                                    fontWeight = FontWeight.Bold,
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
-                                )
-                            },
-                            secondRowContent = {
-                                Text(
-                                    text = data.displayName,
-                                    modifier = Modifier.fillMaxWidth(),
-                                    textAlign = TextAlign.Start,
-                                    fontWeight = FontWeight.Bold,
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
-                                )
-                                Text(
-                                    text = data.symbol,
-                                    modifier = Modifier.fillMaxWidth(),
-                                    textAlign = TextAlign.Start,
-                                    fontWeight = FontWeight.Bold,
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
-                                )
-                            },
-                            thirdRowContent = {
-                                Surface(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    onClick = { onSaveSelectedCurrency(data) }
-                                ) {
-                                    Icon(
-                                        imageVector =
-                                        if (data == currentData) { Icons.Default.CheckCircle }
-                                        else { Icons.AutoMirrored.Outlined.KeyboardArrowRight },
-                                        contentDescription = null,
-                                        tint =
-                                        if (data == currentData) { Color.Green }
-                                        else { MaterialTheme.colorScheme.onTertiaryContainer }
-                                    )
-                                }
-                            }
+    val currentData = configCurrency.configCurrent.currency
+    val preferencesList = configCurrency.currencies
+    Column(
+        modifier = Modifier.fillMaxSize().padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
+            text = "${stringResource(id = str_country)} : ${currentData.displayName}",
+            modifier = Modifier.fillMaxWidth().padding(4.dp),
+            fontWeight = Bold,
+            maxLines = 1,
+            overflow = Ellipsis,
+            textAlign = Center,
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(count = preferencesList.count()){ index ->
+                val data = preferencesList[index]
+                ThreeRowCardItem(
+                    firstRowContent = {
+                        Text(
+                            text = data.symbol,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = Center,
+                            fontWeight = Bold,
+                            maxLines = 1,
+                            overflow = Ellipsis
                         )
+                    },
+                    secondRowContent = {
+                        Text(
+                            text = data.displayName,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = Start,
+                            fontWeight = Bold,
+                            maxLines = 1,
+                            overflow = Ellipsis
+                        )
+                        HorizontalDivider()
+                        Text(
+                            text = data.code,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = Start,
+                            style =  typography.labelSmall,
+                            maxLines = 1,
+                            overflow = Ellipsis
+                        )
+                    },
+                    thirdRowContent = {
+                        Surface(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = { onSaveSelectedCurrency(data) }
+                        ) {
+                            Icon(
+                                imageVector = if (data == currentData) { Default.CheckCircle }
+                                else { Outlined.KeyboardArrowRight },
+                                contentDescription = null,
+                                tint = if (data == currentData) { Green }
+                                else { colorScheme.onTertiaryContainer }
+                            )
+                        }
                     }
-                }
+                )
             }
         }
     }
--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_currency/ui/VMConfGenCurrency.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_currency/ui/VMConfGenCurrency.kt
@@ -1,36 +1,117 @@
 package com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.thomas200593.mini_retail_app.core.data.local.session.SessionState
 import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers.Dispatcher
-import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers.Dispatchers
+import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers.Dispatchers.Dispatchers.IO
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Error
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Idle
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.domain.UCGetConfCurrency
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.entity.ConfigCurrency
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.entity.Currency
-import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.repository.RepoImplConfGenCurrency
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.repository.RepoConfGenCurrency
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui.VMConfGenCurrency.UiEvents.BtnSelectCurrencyEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui.VMConfGenCurrency.UiEvents.ButtonEvents.BtnNavBackEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_currency.ui.VMConfGenCurrency.UiEvents.OnOpenEvents
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class VMConfGenCurrency @Inject constructor(
-    private val repoImplConfGenCurrency: RepoImplConfGenCurrency,
-    ucGetConfCurrency: UCGetConfCurrency,
-    @Dispatcher(Dispatchers.Dispatchers.IO) private val ioDispatcher: CoroutineDispatcher
+    private val repoConfGenCurrency: RepoConfGenCurrency,
+    private val ucGetConfCurrency: UCGetConfCurrency,
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher
 ): ViewModel() {
+    data class UiState(
+        val configCurrency: ResourceState<ConfigCurrency> = Idle,
+        val dialogState: DialogState = DialogState()
+    )
+    data class DialogState(
+        val dlgLoadDataEnabled : MutableState<Boolean> = mutableStateOf(false),
+        val dlgLoadDataErrorEnabled : MutableState<Boolean> = mutableStateOf(false)
+    )
+    sealed class UiEvents {
+        data class OnOpenEvents(val sessionState: SessionState): UiEvents()
+        sealed class ButtonEvents : UiEvents(){
+            sealed class BtnNavBackEvents: ButtonEvents(){
+                data object OnClick: BtnNavBackEvents()
+            }
+        }
+        sealed class BtnSelectCurrencyEvents: ButtonEvents(){
+            data class OnClick(val sessionState: SessionState, val currency: Currency): BtnSelectCurrencyEvents()
+        }
+    }
 
-    val confData = ucGetConfCurrency.invoke().flowOn(ioDispatcher)
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Eagerly,
-            initialValue = ResourceState.Loading
-        )
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
 
-    fun setCurrency(currency: Currency) = viewModelScope.launch {
-        repoImplConfGenCurrency.setCurrency(currency)
+    fun onEvent(events: UiEvents){
+        when(events){
+            is OnOpenEvents -> onOpenEvent(events.sessionState)
+            BtnNavBackEvents.OnClick -> onBtnNavBackClicked()
+            is BtnSelectCurrencyEvents.OnClick ->
+                onSaveSelectedCurrency(events.sessionState, events.currency)
+        }
+    }
+    private fun onOpenEvent(sessionState: SessionState){
+        when(sessionState){
+            SessionState.Loading -> {
+                updateDialogState(
+                    dlgLoadDataEnabled = false,
+                    dlgLoadDataError = false
+                )
+            }
+            is SessionState.Invalid, is SessionState.Valid -> viewModelScope.launch(ioDispatcher) {
+                updateDialogState(
+                    dlgLoadDataEnabled = true,
+                    dlgLoadDataError = false
+                )
+                ucGetConfCurrency.invoke().flowOn(ioDispatcher)
+                    .catch { e ->
+                        _uiState.update { it.copy(configCurrency = Error(e)) }
+                        updateDialogState(
+                            dlgLoadDataEnabled = false,
+                            dlgLoadDataError = true
+                        )
+                    }
+                    .collect{ data ->
+                        _uiState.update { it.copy(configCurrency = data, dialogState = DialogState()) }
+                    }
+            }
+        }
+    }
+    private fun onBtnNavBackClicked() {
+        _uiState.update { it.copy(dialogState = DialogState()) }
+    }
+    private fun onSaveSelectedCurrency(sessionState: SessionState, currency: Currency) {
+        when(sessionState){
+            SessionState.Loading -> Unit
+            is SessionState.Invalid, is SessionState.Valid ->
+                viewModelScope.launch(ioDispatcher) { repoConfGenCurrency.setCurrency(currency) }
+        }
+    }
+    private fun updateDialogState(
+        dlgLoadDataEnabled: Boolean = false,
+        dlgLoadDataError: Boolean = false
+    ) {
+        _uiState.update {
+            it.copy(
+                dialogState = it.dialogState.copy(
+                    dlgLoadDataEnabled = mutableStateOf(dlgLoadDataEnabled),
+                    dlgLoadDataErrorEnabled = mutableStateOf(dlgLoadDataError),
+                )
+            )
+        }
     }
 }
