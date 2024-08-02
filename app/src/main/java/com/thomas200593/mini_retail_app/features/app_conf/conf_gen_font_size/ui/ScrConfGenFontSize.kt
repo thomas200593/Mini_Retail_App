d54a4b5815fee9ced6acbea207f36f569837e54c
thomas200593 <thomas200593@yahoo.com>
2024-08-02 19:49:33 +0700

    Breakdown App Config to Modular & Simplifying Variables

--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_font_size/ui/ScrConfGenFontSize.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_font_size/ui/ScrConfGenFontSize.kt
@@ -8,9 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons.AutoMirrored
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.AutoMirrored.Default
 import androidx.compose.material.icons.Icons.AutoMirrored.Outlined
-import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.filled.CheckCircle
@@ -20,7 +20,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,14 +37,15 @@ import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.thomas200593.mini_retail_app.R
 import com.thomas200593.mini_retail_app.R.string.str_empty_message
 import com.thomas200593.mini_retail_app.R.string.str_empty_message_title
 import com.thomas200593.mini_retail_app.R.string.str_error
 import com.thomas200593.mini_retail_app.R.string.str_error_fetching_preferences
+import com.thomas200593.mini_retail_app.R.string.str_ok
 import com.thomas200593.mini_retail_app.R.string.str_size_font
 import com.thomas200593.mini_retail_app.app.ui.LocalStateApp
 import com.thomas200593.mini_retail_app.app.ui.StateApp
-import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Empty
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Error
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Idle
@@ -52,31 +55,75 @@ import com.thomas200593.mini_retail_app.core.ui.common.CustomIcons.Font.font
 import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar.ProvideTopAppBarAction
 import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar.ProvideTopAppBarNavigationIcon
 import com.thomas200593.mini_retail_app.core.ui.component.CustomAppBar.ProvideTopAppBarTitle
+import com.thomas200593.mini_retail_app.core.ui.component.CustomDialog.AlertDialogContext.ERROR
+import com.thomas200593.mini_retail_app.core.ui.component.CustomDialog.AlertDialogContext.INFORMATION
+import com.thomas200593.mini_retail_app.core.ui.component.CustomDialog.AppAlertDialog
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.EmptyScreen
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.ErrorScreen
-import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.LoadingScreen
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.ThreeRowCardItem
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.entity.ConfigFontSizes
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.entity.FontSize
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui.VMConfGenFontSize.UiEvents.BtnSelectFontSizeEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui.VMConfGenFontSize.UiEvents.ButtonEvents.BtnNavBackEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui.VMConfGenFontSize.UiEvents.OnOpenEvents
 
 @Composable
 fun ScrConfGenFontSize(
-    viewModel: VMConfGenFontSize = hiltViewModel(),
+    vm: VMConfGenFontSize = hiltViewModel(),
     stateApp: StateApp = LocalStateApp.current
 ) {
-    val configData by viewModel.configData.collectAsStateWithLifecycle()
-
-    TopAppBar(onNavigateBack = stateApp::onNavUp)
-    ScreenContent(configData = configData, onSaveSelectedFontSize = viewModel::setFontSize)
+    val uiState by vm.uiState.collectAsStateWithLifecycle()
+    LaunchedEffect(Unit) { vm.onEvent(OnOpenEvents) }
+    TopAppBar(onNavigateBack = { vm.onEvent(BtnNavBackEvents.OnClick).also { stateApp.onNavUp() } })
+    when(uiState.configFontSizes){
+        Idle, Loading -> Unit
+        Empty -> EmptyScreen(
+            title = stringResource(id = str_empty_message_title),
+            emptyMessage = stringResource(id = str_empty_message),
+            showIcon = true
+        )
+        is Error -> ErrorScreen(
+            title = stringResource(id = str_error),
+            errorMessage = stringResource(id = str_error_fetching_preferences),
+            showIcon = true
+        )
+        is Success -> ScreenContent(
+            configFontSize = (uiState.configFontSizes as Success).data,
+            onSaveSelectedFontSize = { vm.onEvent(BtnSelectFontSizeEvents.OnClick(it)) }
+        )
+    }
+    AppAlertDialog(
+        showDialog = uiState.dialogState.dlgLoadDataEnabled,
+        dialogContext = INFORMATION,
+        showIcon = true,
+        showTitle = true,
+        title = { Text(text = stringResource(id = R.string.str_loading))},
+        showBody = true,
+        body = { Text(text = stringResource(id = R.string.str_loading))},
+    )
+    AppAlertDialog(
+        showDialog = uiState.dialogState.dlgLoadDataErrorEnabled,
+        dialogContext = ERROR,
+        showIcon = true,
+        showTitle = true,
+        title = { Text(text = stringResource(id = str_error))},
+        showBody = true,
+        body = { Text("Load Data Error") },
+        useConfirmButton = true,
+        confirmButton = {
+            TextButton(onClick = { vm.onEvent(OnOpenEvents) })
+            { Text(stringResource(id = str_ok)) }
+        }
+    )
 }
 
 @Composable
 private fun TopAppBar(onNavigateBack: () -> Unit) {
     ProvideTopAppBarNavigationIcon {
-        Surface(onNavigateBack, Modifier) {
+        Surface(onClick = onNavigateBack, modifier = Modifier) {
             Icon(
                 modifier = Modifier,
-                imageVector = AutoMirrored.Default.KeyboardArrowLeft,
+                imageVector = Default.KeyboardArrowLeft,
                 contentDescription = null
             )
         }
@@ -107,7 +154,7 @@ private fun TopAppBar(onNavigateBack: () -> Unit) {
         ){
             Icon(
                 modifier = Modifier.sizeIn(maxHeight = IconSize),
-                imageVector = Default.Info,
+                imageVector = Icons.Default.Info,
                 contentDescription = null
             )
         }
@@ -116,83 +163,64 @@ private fun TopAppBar(onNavigateBack: () -> Unit) {
 
 @Composable
 private fun ScreenContent(
-    configData: ResourceState<ConfigFontSizes>,
+    configFontSize: ConfigFontSizes,
     onSaveSelectedFontSize: (FontSize) -> Unit
 ) {
-    when(configData){
-        Idle, Loading -> { LoadingScreen() }
-        Empty -> {
-            EmptyScreen(
-                title = stringResource(id = str_empty_message_title),
-                emptyMessage = stringResource(id = str_empty_message),
-                showIcon = true
-            )
-        }
-        is Error -> {
-            ErrorScreen(
-                title = stringResource(id = str_error),
-                errorMessage = stringResource(id = str_error_fetching_preferences),
-                showIcon = true
-            )
-        }
-        is Success -> {
-            val currentData = configData.data.configCurrent.fontSize
-            val preferencesList = configData.data.fontSizes
+    val currentData = configFontSize.configCurrent.fontSize
+    val preferencesList = configFontSize.fontSizes
 
-            Column(
-                modifier = Modifier.fillMaxSize().padding(4.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "${stringResource(id = str_size_font)} : ${stringResource(id = currentData.title)}",
-                    modifier = Modifier.fillMaxWidth().padding(4.dp),
-                    fontWeight = Bold,
-                    maxLines = 1,
-                    overflow = Ellipsis,
-                    textAlign = Center,
-                )
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize().padding(4.dp),
-                    verticalArrangement = Arrangement.spacedBy(10.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    items(count = preferencesList.count()){ index ->
-                        val data = preferencesList.elementAt(index)
-                        ThreeRowCardItem(
-                            firstRowContent = {
-                                Surface(modifier = Modifier.fillMaxWidth()) {
-                                    Icon(
-                                        imageVector = ImageVector.vectorResource(data.iconRes),
-                                        contentDescription = null
-                                    )
-                                }
-                            },
-                            secondRowContent = {
-                                Text(
-                                    text = stringResource(id = data.title),
-                                    modifier = Modifier.fillMaxWidth(),
-                                    textAlign = Start,
-                                    fontWeight = Bold,
-                                    maxLines = 1,
-                                    overflow = Ellipsis
-                                )
-                            },
-                            thirdRowContent = {
-                                Surface(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    onClick = { onSaveSelectedFontSize(data) }
-                                ) {
-                                    Icon(
-                                        imageVector = if (data == currentData) Default.CheckCircle else Outlined.KeyboardArrowRight,
-                                        contentDescription = null,
-                                        tint = if (data == currentData) Green else colorScheme.onTertiaryContainer
-                                    )
-                                }
-                            }
+    Column(
+        modifier = Modifier.fillMaxSize().padding(4.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "${stringResource(id = str_size_font)} : ${stringResource(id = currentData.title)}",
+            modifier = Modifier.fillMaxWidth().padding(4.dp),
+            fontWeight = Bold,
+            maxLines = 1,
+            overflow = Ellipsis,
+            textAlign = Center,
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(4.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(count = preferencesList.count()){ index ->
+                val data = preferencesList.elementAt(index)
+                ThreeRowCardItem(
+                    firstRowContent = {
+                        Surface(modifier = Modifier.fillMaxWidth()) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(data.iconRes),
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    secondRowContent = {
+                        Text(
+                            text = stringResource(id = data.title),
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = Start,
+                            fontWeight = Bold,
+                            maxLines = 1,
+                            overflow = Ellipsis
                         )
+                    },
+                    thirdRowContent = {
+                        Surface(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = { onSaveSelectedFontSize(data) }
+                        ) {
+                            Icon(
+                                imageVector = if (data == currentData) Icons.Default.CheckCircle else Outlined.KeyboardArrowRight,
+                                contentDescription = null,
+                                tint = if (data == currentData) Green else colorScheme.onTertiaryContainer
+                            )
+                        }
                     }
-                }
+                )
             }
         }
     }
--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_font_size/ui/VMConfGenFontSize.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/conf_gen_font_size/ui/VMConfGenFontSize.kt
@@ -1,31 +1,100 @@
 package com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers.Dispatcher
 import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers.Dispatchers.Dispatchers.IO
-import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Loading
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Error
+import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Idle
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.domain.UCGetConfFontSize
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.entity.ConfigFontSizes
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.entity.FontSize
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.repository.RepoConfGenFontSize
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui.VMConfGenFontSize.UiEvents.BtnSelectFontSizeEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui.VMConfGenFontSize.UiEvents.ButtonEvents.BtnNavBackEvents
+import com.thomas200593.mini_retail_app.features.app_conf.conf_gen_font_size.ui.VMConfGenFontSize.UiEvents.OnOpenEvents
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.SharingStarted.Companion.Eagerly
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class VMConfGenFontSize @Inject constructor(
     private val repoConfGenFontSize: RepoConfGenFontSize,
-    ucGetConfFontSize: UCGetConfFontSize,
+    private val ucGetConfFontSize: UCGetConfFontSize,
     @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher
 ): ViewModel() {
-    val configData = ucGetConfFontSize.invoke().flowOn(ioDispatcher).stateIn(
-        scope = viewModelScope,
-        started = Eagerly,
-        initialValue = Loading
+    data class UiState(
+        val configFontSizes: ResourceState<ConfigFontSizes> = Idle,
+        val dialogState: DialogState = DialogState()
     )
-    fun setFontSize(fontSize: FontSize) = viewModelScope.launch{ repoConfGenFontSize.setFontSize(fontSize) }
+    data class DialogState(
+        val dlgLoadDataEnabled : MutableState<Boolean> = mutableStateOf(false),
+        val dlgLoadDataErrorEnabled : MutableState<Boolean> = mutableStateOf(false)
+    )
+    sealed class UiEvents {
+        data object OnOpenEvents: UiEvents()
+        sealed class ButtonEvents : UiEvents(){
+            sealed class BtnNavBackEvents: ButtonEvents(){
+                data object OnClick: BtnNavBackEvents()
+            }
+        }
+        sealed class BtnSelectFontSizeEvents: ButtonEvents(){
+            data class OnClick(val fontSize: FontSize): BtnSelectFontSizeEvents()
+        }
+    }
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun onEvent(events: UiEvents){
+        when(events){
+            OnOpenEvents -> onOpenEvent()
+            BtnNavBackEvents.OnClick -> onBtnNavBackClicked()
+            is BtnSelectFontSizeEvents.OnClick -> onSaveSelectedFontSize(events.fontSize)
+        }
+    }
+    private fun onOpenEvent() = viewModelScope.launch(ioDispatcher) {
+        updateDialogState(
+            dlgLoadDataEnabled = true,
+            dlgLoadDataError = false
+        )
+        ucGetConfFontSize.invoke().flowOn(ioDispatcher)
+            .catch { e ->
+                _uiState.update { it.copy(configFontSizes = Error(e)) }
+                updateDialogState(
+                    dlgLoadDataEnabled = false,
+                    dlgLoadDataError = true
+                )
+            }
+            .collect{ data ->
+                _uiState.update { it.copy(configFontSizes = data, dialogState = DialogState()) }
+            }
+    }
+    private fun onBtnNavBackClicked() {
+        _uiState.update { it.copy(dialogState = DialogState()) }
+    }
+    private fun onSaveSelectedFontSize(fontSize: FontSize) =
+        viewModelScope.launch(ioDispatcher) { repoConfGenFontSize.setFontSize(fontSize) }
+    private fun updateDialogState(
+        dlgLoadDataEnabled: Boolean = false,
+        dlgLoadDataError: Boolean = false
+    ) {
+        _uiState.update {
+            it.copy(
+                dialogState = it.dialogState.copy(
+                    dlgLoadDataEnabled = mutableStateOf(dlgLoadDataEnabled),
+                    dlgLoadDataErrorEnabled = mutableStateOf(dlgLoadDataError),
+                )
+            )
+        }
+    }
 }
