90e7f5fe1869e57f83ae99fffe2e7d7f5050284c
thomas200593 <thomas200593@yahoo.com>
2024-08-07 13:14:45 +0700

    Restructuring Business Profile to Conf

--- app/src/main/java/com/thomas200593/mini_retail_app/features/business/biz_profile/domain/UCGetBizProfileShort.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/business/biz_profile/domain/UCGetBizProfileShort.kt
@@ -4,7 +4,6 @@ import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers
 import com.thomas200593.mini_retail_app.core.design_system.coroutine_dispatchers.Dispatchers.Dispatchers.IO
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Empty
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Error
-import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Loading
 import com.thomas200593.mini_retail_app.core.design_system.util.ResourceState.Success
 import com.thomas200593.mini_retail_app.features.business.biz_profile.entity.toBizProfileShort
 import com.thomas200593.mini_retail_app.features.business.biz_profile.repository.RepoBizProfile
@@ -12,7 +11,6 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class UCGetBizProfileShort @Inject constructor(
@@ -27,5 +25,4 @@ class UCGetBizProfileShort @Inject constructor(
             }
         }
         .catch { Error(it) }
-        .onStart { Loading }
 }
