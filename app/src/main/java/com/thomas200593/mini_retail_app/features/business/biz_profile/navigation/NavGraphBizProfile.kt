2a77823d7b669baf1953df437ca510a4f9c03063
thomas200593 <thomas200593@yahoo.com>
2024-08-14 22:44:38 +0700

    Do the Business Profile Detail & Update things

--- app/src/main/java/com/thomas200593/mini_retail_app/features/business/biz_profile/navigation/NavGraphBizProfile.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/business/biz_profile/navigation/NavGraphBizProfile.kt
@@ -1,9 +1,7 @@
 package com.thomas200593.mini_retail_app.features.business.biz_profile.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.navOptions
 import androidx.navigation.navigation
 import com.thomas200593.mini_retail_app.app.navigation.NavGraph.G_BIZ_PROFILE
 import com.thomas200593.mini_retail_app.app.navigation.ScrGraphs.BizProfile
@@ -18,14 +16,4 @@ fun NavGraphBuilder.navGraphBizProfile(){
             route = BizProfile.route
         ){ ScrBizProfile() }
     }
-}
-
-fun NavController.navToBizProfile(
-    destBizProfile: DestBizProfile?
-) {
-    val navOptions = navOptions { launchSingleTop = true; restoreState = true }
-    this.navigate(
-        route = destBizProfile?.route?: G_BIZ_PROFILE,
-        navOptions = navOptions
-    )
 }
