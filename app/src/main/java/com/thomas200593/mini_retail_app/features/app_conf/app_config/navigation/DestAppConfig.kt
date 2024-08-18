d1be481e38b420f7ece2bee07a39c45afad88291
thomas200593 <thomas200593@yahoo.com>
2024-08-18 07:17:19 +0700

    Do the Business Profile Detail & Update things

--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/app_config/navigation/DestAppConfig.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/app_config/navigation/DestAppConfig.kt
@@ -3,6 +3,7 @@ package com.thomas200593.mini_retail_app.features.app_conf.app_config.navigation
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.thomas200593.mini_retail_app.R.string
+import com.thomas200593.mini_retail_app.app.navigation.ScrGraphs
 import com.thomas200593.mini_retail_app.app.navigation.ScrGraphs.ConfigData
 import com.thomas200593.mini_retail_app.app.navigation.ScrGraphs.ConfigGeneral
 import com.thomas200593.mini_retail_app.core.ui.common.CustomIcons.Setting.settings_data
@@ -29,4 +30,9 @@ enum class DestAppConfig(
         description = string.str_configuration_data,
         usesAuth = true
     )
+}
+
+enum class DestAppConfig2(val scrGraphs: ScrGraphs){
+    CONFIG_GENERAL(scrGraphs = ConfigGeneral),
+    CONFIG_DATA(scrGraphs = ConfigData)
 }
--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/app_config/navigation/NavGraphAppConfig.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/app_config/navigation/NavGraphAppConfig.kt
@@ -7,9 +7,9 @@ import androidx.navigation.navOptions
 import androidx.navigation.navigation
 import com.thomas200593.mini_retail_app.app.navigation.NavGraph.G_APP_CONFIG
 import com.thomas200593.mini_retail_app.app.navigation.ScrGraphs.AppConfig
+import com.thomas200593.mini_retail_app.features.app_conf.app_config.ui.ScrAppConfig
 import com.thomas200593.mini_retail_app.features.app_conf.conf_data.navigation.navGraphConfData
 import com.thomas200593.mini_retail_app.features.app_conf.conf_gen.navigation.navGraphConfGen
-import com.thomas200593.mini_retail_app.features.app_conf.app_config.ui.ScrAppConfig
 
 fun NavGraphBuilder.navGraphAppConfig(){
     navigation(
@@ -27,7 +27,7 @@ fun NavGraphBuilder.navGraphAppConfig(){
 fun NavController.navToAppConfig(destAppConfig: DestAppConfig?){
     val navOptions = navOptions { launchSingleTop = true; restoreState = true }
     this.navigate(
-        route = destAppConfig?.route?: G_APP_CONFIG,
+        route = destAppConfig?.route ?: G_APP_CONFIG,
         navOptions = navOptions
     )
 }
