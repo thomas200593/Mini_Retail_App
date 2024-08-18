576d79946400585d3982ac65796383628f4cb479
thomas200593 <thomas200593@yahoo.com>
2024-08-18 16:45:09 +0700

    Do the Business Profile Detail & Update things

--- app/src/main/java/com/thomas200593/mini_retail_app/features/onboarding/ui/ScrOnboarding.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/onboarding/ui/ScrOnboarding.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush.Companion.verticalGradient
@@ -52,23 +53,43 @@ import com.thomas200593.mini_retail_app.features.onboarding.ui.VMOnboarding.UiEv
 import com.thomas200593.mini_retail_app.features.onboarding.ui.VMOnboarding.UiEvents.TabsEvents.TabPageSelection
 import com.thomas200593.mini_retail_app.features.onboarding.ui.VMOnboarding.UiStateOnboardingPages.Loading
 import com.thomas200593.mini_retail_app.features.onboarding.ui.VMOnboarding.UiStateOnboardingPages.Success
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScrOnboarding(
     vm: VMOnboarding = hiltViewModel(),
     stateApp: StateApp = LocalStateApp.current
 ){
+    val coroutineScope = rememberCoroutineScope()
     val uiState by vm.uiState.collectAsStateWithLifecycle()
     LaunchedEffect(Unit) {vm.onEvent(OnOpenEvents)}
+    ScrOnboarding(
+        uiState = uiState,
+        onBtnNextClicked = { vm.onEvent(ButtonNextEvents.OnClick) },
+        onTabSelected = { vm.onEvent(TabPageSelection.OnSelect(it)) },
+        onFinishedOnboarding = {
+            vm.onEvent(OnFinishedOnboardingEvent).also {
+                coroutineScope.launch { stateApp.navController.navToInitialization() }
+            }
+        }
+    )
+}
+
+@Composable
+private fun ScrOnboarding(
+    uiState: VMOnboarding.UiState,
+    onBtnNextClicked: () -> Unit,
+    onTabSelected: (Int) -> Unit,
+    onFinishedOnboarding: () -> Unit
+) {
     when(uiState.onboardingPages){
         Loading -> LoadingScreen()
         is Success -> ScreenContent(
-            onboardingPages = (uiState.onboardingPages as Success).onboardingPages,
+            onboardingPages = uiState.onboardingPages.onboardingPages,
             screenState = uiState.screenState,
-            onBtnNextClicked = { vm.onEvent(ButtonNextEvents.OnClick) },
-            onTabSelected = { vm.onEvent(TabPageSelection.OnSelect(it)) },
-            onFinishedOnboarding =
-            { vm.onEvent(OnFinishedOnboardingEvent).also { stateApp.navController.navToInitialization() } }
+            onBtnNextClicked = onBtnNextClicked,
+            onTabSelected = onTabSelected,
+            onFinishedOnboarding = onFinishedOnboarding
         )
     }
 }
@@ -81,17 +102,25 @@ private fun ScreenContent(
     onTabSelected: (Int) -> Unit,
     onFinishedOnboarding: () -> Unit
 ) {
-    Column(modifier = Modifier.fillMaxSize().testTag(TAG_ONBOARD_SCREEN)) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .testTag(TAG_ONBOARD_SCREEN)) {
         OnboardingImages(
-            modifier = Modifier.weight(1.0f).fillMaxWidth(),
+            modifier = Modifier
+                .weight(1.0f)
+                .fillMaxWidth(),
             currentPage = onboardingPages[screenState.currentPage]
         )
         OnboardingDetails(
-            modifier = Modifier.weight(1.0f).padding(16.dp),
+            modifier = Modifier
+                .weight(1.0f)
+                .padding(16.dp),
             currentPage = onboardingPages[screenState.currentPage]
         )
         OnboardingNavigation(
-            modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 16.dp),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 16.dp),
             currentPage = screenState.currentPage,
             pageSize = onboardingPages.size,
             onBtnNextClicked = onBtnNextClicked,
@@ -115,8 +144,18 @@ private fun OnboardingImages(modifier: Modifier, currentPage: OnboardingPage) {
             contentScale = FillWidth,
             modifier = Modifier.fillMaxSize()
         )
-        Box(modifier = Modifier.fillMaxSize().align(Alignment.BottomCenter).graphicsLayer{alpha=0.6f}
-            .background(verticalGradient(colorStops = arrayOf(Pair(0.8f, Transparent), Pair(1f, White))))
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .align(Alignment.BottomCenter)
+            .graphicsLayer { alpha = 0.6f }
+            .background(
+                verticalGradient(
+                    colorStops = arrayOf(
+                        Pair(0.8f, Transparent),
+                        Pair(1f, White)
+                    )
+                )
+            )
         )
     }
 }
@@ -172,7 +211,10 @@ private fun OnboardingTabSelector(
 ) {
     TabRow(
         selectedTabIndex = currentPage,
-        modifier = modifier.fillMaxWidth().background(colorScheme.primary).testTag(TAG_ONBOARD_TAG_ROW)
+        modifier = modifier
+            .fillMaxWidth()
+            .background(colorScheme.primary)
+            .testTag(TAG_ONBOARD_TAG_ROW)
     ) {
         onboardingPages.forEachIndexed { index, _ ->
             Tab(
@@ -181,11 +223,13 @@ private fun OnboardingTabSelector(
                 onClick = { onTabSelected.invoke(index) }
             ) {
                 Box(
-                    modifier = Modifier.testTag("$TAG_ONBOARD_TAG_ROW$index").size(8.dp)
-                    .background(
-                        color = if (index == currentPage) colorScheme.onPrimary else LightGray,
-                        shape = RoundedCornerShape(4.dp)
-                    )
+                    modifier = Modifier
+                        .testTag("$TAG_ONBOARD_TAG_ROW$index")
+                        .size(8.dp)
+                        .background(
+                            color = if (index == currentPage) colorScheme.onPrimary else LightGray,
+                            shape = RoundedCornerShape(4.dp)
+                        )
                 )
             }
         }
