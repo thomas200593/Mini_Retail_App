edf46bb1d4b01176d5635995a32fd83e26205f19
thomas200593 <thomas200593@yahoo.com>
2024-08-31 13:59:46 +0700

    Handle onboarding flow to language aware

--- app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/app_config/ui/VMAppConfig.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/app_conf/app_config/ui/VMAppConfig.kt
@@ -123,9 +123,7 @@ class VMAppConfig @Inject constructor(
                 repoAppConf.getMenuData().flowOn(ioDispatcher).collectLatest { menuData ->
                     _uiState.update {
                         it.copy(
-                            destAppConfig = Success(
-                                destAppConfig = menuData
-                            ),
+                            destAppConfig = Success(destAppConfig = menuData),
                             dialogState = DialogState()
                         )
                     }
