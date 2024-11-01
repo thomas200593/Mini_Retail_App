c8bfea0a6a386352ef2ff4c7a572ca0b8fc78a9d
thomas200593 <thomas200593@yahoo.com>
2024-11-01 18:32:38 +0700

    + Refactoring Dialogs

--- app/src/main/java/com/thomas200593/mini_retail_app/work/workers/session_monitor/manager/ManagerWorkSessionMonitor.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/work/workers/session_monitor/manager/ManagerWorkSessionMonitor.kt
@@ -2,13 +2,13 @@ package com.thomas200593.mini_retail_app.work.workers.session_monitor.manager
 
 import android.content.Context
 import androidx.work.ExistingPeriodicWorkPolicy.UPDATE
-import androidx.work.WorkManager.getInstance
+import androidx.work.WorkManager
 import com.thomas200593.mini_retail_app.work.workers.session_monitor.worker.WorkerSessionMonitor.Companion.startUpWork
 
 object ManagerWorkSessionMonitor{
     private const val WORK_NAME: String = "ManagerWorkSessionMonitor"
     fun initialize(context: Context){
-        getInstance(context).apply {
+        WorkManager.getInstance(context).apply {
             enqueueUniquePeriodicWork(
                 WORK_NAME,
                 UPDATE,
@@ -16,5 +16,5 @@ object ManagerWorkSessionMonitor{
             )
         }
     }
-    fun terminate(context: Context){ getInstance(context).cancelUniqueWork(WORK_NAME) }
+    fun terminate(context: Context){ WorkManager.getInstance(context).cancelUniqueWork(WORK_NAME) }
 }
