7ecb323465efe002e7b57a5fcb7b8482d2277f8e
thomas200593 <thomas200593@yahoo.com>
2024-11-02 21:17:22 +0700

    + Refactoring Dialogs

--- app/src/main/java/com/thomas200593/mini_retail_app/features/reporting/ui/ScrReporting.kt
+++ app/src/main/java/com/thomas200593/mini_retail_app/features/reporting/ui/ScrReporting.kt
@@ -10,11 +10,9 @@ import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,18 +28,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.thomas200593.mini_retail_app.R
 import com.thomas200593.mini_retail_app.app.navigation.ScrGraphs
 import com.thomas200593.mini_retail_app.app.ui.LocalStateApp
 import com.thomas200593.mini_retail_app.app.ui.StateApp
 import com.thomas200593.mini_retail_app.core.ui.common.CustomThemes
-import com.thomas200593.mini_retail_app.core.ui.component.app_bar.CustomAppBar.ProvideTopAppBarAction
-import com.thomas200593.mini_retail_app.core.ui.component.app_bar.CustomAppBar.ProvideTopAppBarTitle
-import com.thomas200593.mini_retail_app.core.ui.component.CustomButton.Common.AppIconButton
-import com.thomas200593.mini_retail_app.core.ui.component.dialog.CustomDialog.AlertDialogContext
-import com.thomas200593.mini_retail_app.core.ui.component.dialog.CustomDialog.AppAlertDialog
 import com.thomas200593.mini_retail_app.core.ui.component.CustomPanel.LoadingScreen
 import com.thomas200593.mini_retail_app.core.ui.component.CustomScreenUtil.LockScreenOrientation
+import com.thomas200593.mini_retail_app.core.ui.component.app_bar.CustomAppBar.ProvideTopAppBarAction
+import com.thomas200593.mini_retail_app.core.ui.component.app_bar.CustomAppBar.ProvideTopAppBarTitle
+import com.thomas200593.mini_retail_app.core.ui.component.dialog.DlgError
+import com.thomas200593.mini_retail_app.core.ui.component.dialog.DlgInformation
 import com.thomas200593.mini_retail_app.features.reporting.ui.VMReporting.UiEvents.ButtonEvents.BtnScrDescEvents
 import com.thomas200593.mini_retail_app.features.reporting.ui.VMReporting.UiEvents.OnOpenEvents
 import com.thomas200593.mini_retail_app.features.reporting.ui.VMReporting.UiState
@@ -107,54 +103,14 @@ private fun HandleDialogs(
     onDismissDlgScrDesc: () -> Unit,
     onDismissDlgSessionInvalid: () -> Unit
 ) {
-    AppAlertDialog(
-        showDialog = uiState.dialogState.dlgSessionInvalid,
-        dialogContext = AlertDialogContext.ERROR,
-        showTitle = true,
-        title = { Text(text = stringResource(id = R.string.str_error)) },
-        showBody = true,
-        body = {
-            Column(
-                modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState()),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) { Text(text = stringResource(id = R.string.str_deny_access_auth_required)) }
-        },
-        useDismissButton = true,
-        dismissButton = {
-            AppIconButton(
-                onClick = onDismissDlgSessionInvalid,
-                icon = Icons.Default.Close,
-                text = stringResource(id = R.string.str_close),
-                containerColor = MaterialTheme.colorScheme.error,
-                contentColor = MaterialTheme.colorScheme.onError
-            )
-        }
-    )
-    AppAlertDialog(
+    DlgInformation.ScrDesc(
         showDialog = uiState.dialogState.dlgScrDesc,
-        dialogContext = AlertDialogContext.INFORMATION,
-        showIcon = true,
-        showTitle = true,
-        title = { currentScreen.title?.let { Text(text = stringResource(id = it)) } },
-        showBody = true,
-        body = {
-            currentScreen.description?.let {
-                Column(
-                    modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) { Text(text = stringResource(id = it)) }
-            }
-        },
-        useDismissButton = true,
-        dismissButton = {
-            AppIconButton(
-                onClick = onDismissDlgScrDesc,
-                icon = Icons.Default.Close,
-                text = stringResource(id = R.string.str_close)
-            )
-        }
+        currentScreen = currentScreen,
+        onDismiss = onDismissDlgScrDesc
+    )
+    DlgError.SessionInvalid(
+        showDialog = uiState.dialogState.dlgSessionInvalid,
+        onDismiss = onDismissDlgSessionInvalid
     )
 }
 
